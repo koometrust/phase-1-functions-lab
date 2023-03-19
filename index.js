e773f2a0b5831e175a8440a35642c01d9fff80ea
Koome <judekoome360@gmail.com>
2023-03-20 00:40:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+  }
+  
+  function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks) * 264;
+  }
+  
+  function distanceTravelledInFeet(startBlock, endBlock) {
+    let distanceInBlocks = Math.abs(endBlock - startBlock);
+    let distanceInFeet = distanceInBlocks * 264;
+    return distanceInFeet;
+    }
+    function calculatesFarePrice(start, destination) {
+        let distanceInFeet = distanceTravelledInFeet(start, destination);
+        let farePrice;
+      
+        if (distanceInFeet <= 400) {
+          farePrice = 0;
+        } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+          farePrice = (distanceInFeet - 400) * 0.02;
+        } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+          farePrice = 25;
+        } else {
+          farePrice = 'cannot travel that far';
+        }
+      
+        return farePrice;
+      }
+      
